0a555d69613479fd4eafe243fbe9d8381321367f
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-01-17 22:56:51 +0530

    ff

---
+++ index.js
@@ -0,0 +1,88 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let words = [];
+let group, wallLeft, wallRight;
+let wordsToDisplay = [
+    "discord.js",
+    "react.js",
+    "next.js",
+    "node.js",
+    "mongo.js",
+    "gsap.js"
+];
+
+function setup() {
+    createCanvas(innerWidth, innerHeight - 5);
+    
+    engine = Engine.create();
+
+    group = Bodies.rectangle(width / 2, height - 20, width, 10, {
+        isStatic: true,
+    });
+    wallLeft = Bodies.rectangle(0, height / 2, 10, height, {
+        isStatic: true,
+    });
+    wallRight = Bodies.rectangle(width, height / 2, 10, height, {
+        isStatic: true,
+    });
+
+    World.add(engine.world, [group, wallLeft, wallRight]);
+
+    for (let i = 0; i < wordsToDisplay.length; i++) {
+        words.push(new Word(random(width), -200, wordsToDisplay[i]));
+    }
+}
+
+function draw() {
+    background("#ffff60");
+    Engine.update(engine);
+    for (let word of words) {
+        word.show();
+    }
+}
+
+class Word {
+    constructor(x, y, word) {
+        this.body = Bodies.rectangle(x, y, word.length * 20, 40);
+        this.word = word;
+        World.add(engine.world, this.body);
+    }
+
+    show() {
+        let pos = this.body.position;
+        let angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+
+        rectMode(CENTER);
+        fill(255);
+        stroke("#111222");
+        strokeWeight(3);
+        rect(0, 0, this.word.length * 20, 40);
+        noStroke();
+        fill("#0f0f0f");
+        textAlign(CENTER, CENTER);
+        text(this.word.toUpperCase(), 0, 0);
+
+        pop();
+    }
+}
+
+function mouseMoved() {
+    for (let word of words) {
+        let distance = dist(mouseX, mouseY, word.body.position.x, word.body.position.y);
+        if (distance < 100) {
+            Body.applyForce(
+                word.body,
+                { x: word.body.position.x, y: word.body.position.y },
+                { x: random(-0.2, 0.2), y: random(-0.2, 0.2) }
+            );
+        }
+    }
+}
